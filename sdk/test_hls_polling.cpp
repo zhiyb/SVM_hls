d2513193ca3eec644a85be2bad4f0efd2e9191aa
Yubo Zhi <normanzyb@gmail.com>
2017-03-15 22:00:48 +0000

    SDK HLS classifier detailed performance data

--- sdk/test_hls_polling.cpp
+++ sdk/test_hls_polling.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <xtime_l.h>
 #include "test.h"
 
 void test_cls_hls_polling_pre()
@@ -8,20 +10,29 @@ void test_cls_hls_polling_pre()
 
 unsigned int test_cls_hls_polling()
 {
+	XTime tick[4], perf[3] = {0, 0, 0};
 	unsigned int err = 0;
 	int *label = &testDataLabel[0];
 	data_t *x = &testDataI[0][0];
 	static XClassifier_X_v data;
 	for (size_t ix = ASIZE(testDataLabel); ix != 0; ix--) {
+		XTime_GetTime(&tick[0]);
 		u32 *p = &data.word_0;
 		for (size_t i = N / 2; i != 0; i--) {
 			*p++ = (*x) | (*(x + 1) << 16);
 			x += 2;
 		}
 		XClassifier_Set_x_V(&cls, data);
+		XTime_GetTime(&tick[1]);
 		XClassifier_Start(&cls);
+		XTime_GetTime(&tick[2]);
 		while (!XClassifier_IsReady(&cls));
+		XTime_GetTime(&tick[3]);
 		err += (!XClassifier_Get_output_r(&cls)) != (!*label++);
+		perf[0] += tick[1] - tick[0];
+		perf[1] += tick[2] - tick[1];
+		perf[2] += tick[3] - tick[2];
 	}
+	printf("Data %llu, starting %llu, result %llu\r\n", perf[0], perf[1], perf[2]);
 	return err;
 }
